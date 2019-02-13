e0cbf70bf11c26daf34adee710362fe32ee7e957
LuizaTaysumova <36314072+LuizaTaysumova@users.noreply.github.com>
2019-02-13 15:07:28 +0300

    in the first task script based on ids of figures was changed on their data attributes. in the second task the method map was changed to forEach()/changes after the teacher's review

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -1,18 +1,6 @@
 $(document).ready(() => {
     $('button').on('click', (e) => {
         $('.window').children().hide();
-        switch(e.target.id) {
-            case 'first_btn': $('#p_1').slideDown();
-                break;
-            case 'second_btn': $('#p_2').slideDown();
-                break;
-            case 'third_btn': $('#p_3').slideDown();
-                break;
-            case 'fourth_btn': $('#p_4').slideDown();
-                break;
-            case 'fifth_btn': $('#p_5').slideDown();
-                break;
-        }
-        console.log(event.target.id);
+        $(`[data-btn='${e.target.id}']`).slideDown();
     });
 });
